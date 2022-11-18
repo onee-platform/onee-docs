6f00f7c0275d740ac636bc2c53873e9ed4d45656
Bendt Indonesia <46597618+bendt-indonesia@users.noreply.github.com>
2022-11-18 15:11:38 +0700

    chek

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -34,7 +34,7 @@ const config = {
       ({
         docs: {
           routeBasePath: '/', // Serve the docs at the site's root
-          // sidebarPath: require.resolve('./sidebars.js'),
+          sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           // editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
